61e22e3b615f659cdfae2645db7e1d45ffed3a5b
vforvickyxia <vicky.xiayutong@gmail.com>
2024-10-03 19:43:47 -0400

    Initial Commit

---
+++ SwiftyHalloween/SwiftyHalloweenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyHalloweenApp.swift
+//  SwiftyHalloween
+//
+//  Created by Vicky Xia on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyHalloweenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftyHalloweenTests/SwiftyHalloweenTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyHalloweenTests.swift
+//  SwiftyHalloweenTests
+//
+//  Created by Vicky Xia on 10/3/24.
+//
+
+import Testing
+@testable import SwiftyHalloween
+
+struct SwiftyHalloweenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
